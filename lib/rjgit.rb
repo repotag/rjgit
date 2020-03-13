f5d0c107a5ba179528649abf6f34e758135c8323
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-03-13 17:40:32 +0100

    Add specs and better error handling. Refactor RJGit::Porcelain.diff

--- lib/rjgit.rb
+++ lib/rjgit.rb
@@ -10,7 +10,7 @@ module RJGit
   def self.version
     VERSION
   end
-  
+
   require 'uri'
   require 'stringio'
   # gem requires
@@ -19,13 +19,13 @@ module RJGit
   require "#{File.dirname(__FILE__)}/rjgit_helpers.rb"
   # require everything else
   begin
-    Dir["#{File.dirname(__FILE__)}/*.rb"].each do |file| 
+    Dir["#{File.dirname(__FILE__)}/*.rb"].each do |file|
       require file
     end
   end
-  
+
   import 'org.eclipse.jgit.lib.ObjectId'
-   
+
   module Porcelain
 
     import 'org.eclipse.jgit.lib.Constants'
@@ -36,20 +36,20 @@ module RJGit
     import 'org.eclipse.jgit.blame.BlameResult'
     import 'org.eclipse.jgit.treewalk.CanonicalTreeParser'
     import 'org.eclipse.jgit.diff.DiffFormatter'
-    
+
     # http://wiki.eclipse.org/JGit/User_Guide#Porcelain_API
     def self.add(repository, file_pattern)
       repository.add(file_pattern)
     end
-    
+
     def self.commit(repository, message="")
       repository.commit(message)
     end
-    
+
     def self.object_for_tag(repository, tag)
       repository.find(tag.object.name, RJGit.sym_for_type(tag.object_type))
     end
-    
+
     # http://dev.eclipse.org/mhonarc/lists/jgit-dev/msg00558.html
     def self.cat_file(repository, blob)
       jrepo = RJGit.repository_type(repository)
@@ -65,7 +65,7 @@ module RJGit
       bytes = jrepo.open(jblob.id).get_bytes
       return bytes.to_a.pack('c*').force_encoding('UTF-8')
     end
-    
+
     def self.ls_tree(repository, path=nil, treeish=Constants::HEAD, options={})
       options = {recursive: false, print: false, io: $stdout, path_filter: nil}.merge options
       jrepo = RJGit.repository_type(repository)
@@ -95,7 +95,7 @@ module RJGit
       treewalk.set_recursive(options[:recursive])
       treewalk.set_filter(PathFilter.create(options[:path_filter])) if options[:path_filter]
       entries = []
-    
+
       while treewalk.next
         entry = {}
         mode = treewalk.get_file_mode(0)
@@ -107,8 +107,8 @@ module RJGit
       end
       options[:io].puts RJGit.stringify(entries) if options[:print]
       entries
-    end  
-      
+    end
+
     def self.blame(repository, file_path, options={})
       options = {:print => false, :io => $stdout}.merge(options)
       jrepo = RJGit.repository_type(repository)
@@ -130,28 +130,22 @@ module RJGit
       options[:io].puts RJGit.stringify(blame) if options[:print]
       return blame
     end
-    
+
     def self.diff(repository, options = {})
       options = {:namestatus => false, :patch => false}.merge(options)
       repo = RJGit.repository_type(repository)
       git = RubyGit.new(repo).jgit
       diff_command = git.diff
-        if options[:old_rev]
+      [:old, :new].each do |which_rev|
+        if rev = options["#{which_rev}_rev".to_sym]
           reader = repo.new_object_reader
-          old_tree = repo.resolve("#{options[:old_rev]}^{tree}")
-          old_tree_iter = CanonicalTreeParser.new
-          old_tree_iter.reset(reader, old_tree)
-          diff_command.set_old_tree(old_tree_iter)
+          parser = CanonicalTreeParser.new
+          parser.reset(reader, repo.resolve("#{rev}^{tree}"))
+          diff_command.send("set_#{which_rev}_tree".to_sym, parser)
         end
-        if options[:new_rev]
-          reader = repo.new_object_reader unless reader
-          new_tree = repo.resolve("#{options[:new_rev]}^{tree}")
-          new_tree_iter = CanonicalTreeParser.new
-          new_tree_iter.reset(reader, new_tree)
-          diff_command.set_new_tree(new_tree_iter)
-        end
+      end
       diff_command.set_path_filter(PathFilter.create(options[:file_path])) if options[:file_path]
-      diff_command.set_show_name_and_status_only(true) if options[:namestatus] 
+      diff_command.set_show_name_and_status_only(true) if options[:namestatus]
       diff_command.set_cached(true) if options[:cached]
       diff_entries = diff_command.call
       diff_entries = diff_entries.to_array.to_ary
@@ -169,34 +163,34 @@ module RJGit
       diff_entries = options[:patch] ? result : diff_entries.map {|entry| [entry]}
       RJGit.convert_diff_entries(diff_entries)
     end
-    
+
   end
-  
+
   module Plumbing
     import org.eclipse.jgit.lib.Constants
-    
+
     class TreeBuilder
       import org.eclipse.jgit.lib.FileMode
       import org.eclipse.jgit.lib.TreeFormatter
       import org.eclipse.jgit.patch.Patch
-    
+
       attr_accessor :treemap
       attr_reader :log
-      
+
       def initialize(repository)
         @jrepo = RJGit.repository_type(repository)
         @treemap = {}
         init_log
       end
-      
+
       def object_inserter
         @object_inserter ||= @jrepo.newObjectInserter
       end
-      
+
       def init_log
         @log = {:deleted => [], :added => [] }
       end
-      
+
       def only_contains_deletions(hashmap)
         hashmap.each do |key, value|
           if value.is_a?(Hash)
@@ -207,7 +201,7 @@ module RJGit
         end
         true
       end
-      
+
       def build_tree(start_tree, treemap = nil, flush = false)
         existing_trees = {}
         untouched_objects = {}
@@ -232,7 +226,7 @@ module RJGit
             end
           end
         end
-        
+
         sorted_treemap = treemap.inject({}) {|h, (k,v)| v.is_a?(Hash) ? h["#{k}/"] = v : h[k] = v; h }.merge(untouched_objects).sort
         sorted_treemap.each do |object_name, data|
           case data
@@ -252,27 +246,27 @@ module RJGit
         end
         object_inserter.insert(formatter)
       end
-      
+
       def write_blob(contents, flush = false)
         blobid = object_inserter.insert(Constants::OBJ_BLOB, contents.to_java_bytes)
         object_inserter.flush if flush
         blobid
       end
-      
+
     end
-    
+
     class Index
       import org.eclipse.jgit.lib.CommitBuilder
-      
+
       attr_accessor :treemap, :current_tree
       attr_reader :jrepo
-      
+
       def initialize(repository)
         @treemap = {}
         @jrepo = RJGit.repository_type(repository)
         @treebuilder = TreeBuilder.new(@jrepo)
       end
-      
+
       def add(path, data)
         path = path[1..-1] if path[0] == '/'
         path = path.split('/')
@@ -289,24 +283,24 @@ module RJGit
         current[filename] = data
         @treemap
       end
-  
+
       def delete(path)
         path = path[1..-1] if path[0] == '/'
         path = path.split('/')
         last = path.pop
-    
+
         current = self.treemap
-    
+
         path.each do |dir|
           current[dir] ||= {}
           node = current[dir]
           current = node
         end
-    
+
         current[last] = false
         @treemap
       end
-      
+
       def do_commit(message, author, parents, new_tree)
         commit_builder = CommitBuilder.new
         person = author.person_ident
@@ -323,13 +317,13 @@ module RJGit
         @treebuilder.object_inserter.flush
         result
       end
-      
+
       def commit(message, author, parents = nil, ref = "refs/heads/#{Constants::MASTER}", force = false)
         @current_tree = @current_tree ? RJGit.tree_type(@current_tree) : @jrepo.resolve("#{ref}^{tree}")
         @treebuilder.treemap = @treemap
         new_tree = @treebuilder.build_tree(@current_tree)
         return false if @current_tree && new_tree.name == @current_tree.name
-        
+
         parents = parents ? parents : @jrepo.resolve(ref+"^{commit}")
         new_head = do_commit(message, author, parents, new_tree)
 
@@ -340,18 +334,18 @@ module RJGit
         ru.setRefLogIdent(author.person_ident)
         ru.setRefLogMessage("commit: #{message}", false)
         res = ru.update.to_string
-        
+
         @current_tree = new_tree
         log = @treebuilder.log
         @treebuilder.init_log
         sha =  ObjectId.to_string(new_head)
         return res, log, sha
       end
-      
+
       def self.successful?(result)
         ["NEW", "FAST_FORWARD", "FORCED"].include?(result)
       end
-      
+
     end
 
     class ApplyPatchToIndex < RJGit::Plumbing::Index
@@ -365,12 +359,19 @@ module RJGit
       DELETE = org.eclipse.jgit.diff.DiffEntry::ChangeType::DELETE
       RENAME = org.eclipse.jgit.diff.DiffEntry::ChangeType::RENAME
 
+      # Take the result of RJGit::Porcelain.diff with options[:patch] = true and return a patch String
+      def self.diffs_to_patch(diffs)
+        result = ""
+        diffs.each {|diff| result << diff[:patch]}
+        result
+      end
+
       def initialize(repository, patch, ref = Constants::HEAD)
         super(repository)
         @ref   = ref
         @patch = Patch.new
         @patch.parse(ByteArrayInputStream.new(patch.to_java_bytes))
-        raise 'Error parsing patch' unless @patch.getErrors.empty?
+        raise_patch_apply_error unless @patch.getErrors.to_a.empty?
         @current_tree = Commit.find_head(@jrepo, ref).tree
       end
 
@@ -379,6 +380,7 @@ module RJGit
       end
 
       def build_map
+        raise_patch_apply_error if @patch.getFiles.to_a.empty?
         @patch.getFiles.each do |file_header|
           case file_header.getChangeType
           when ADD
@@ -398,12 +400,20 @@ module RJGit
 
       private
 
+      def raise_patch_apply_error
+        raise Java::OrgEclipseJgitApiErrors::PatchApplyException.new('Patch failed to apply')
+      end
+
       def getData(path)
-        (@current_tree / path).data
+        begin
+          (@current_tree / path).data
+        rescue
+          raise_patch_apply_error
+        end
       end
 
       def hunk_sanity_check(hunk, hunk_line, pos, newLines)
-        raise 'Could not build treemap: the patch failed to apply' unless newLines[hunk.getNewStartLine - 1 + pos] == hunk_line[1..-1]
+        raise_patch_apply_error unless newLines[hunk.getNewStartLine - 1 + pos] == hunk_line[1..-1]
       end
 
       def apply(original, file_header)
@@ -433,7 +443,7 @@ module RJGit
         newLines.join
       end
     end
-    
+
   end
-  
+
 end
